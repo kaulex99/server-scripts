a7808774e10be3ae9cd1aaf5ea5c2b33629f3f29
kaulex <contact@kaulex.dev>
2021-07-31 21:17:13 +0200

    fixed nextcloud backup script

--- nextcloud-backup.sh
+++ nextcloud-backup.sh
@@ -9,14 +9,9 @@ nextcloud_db_password=""
 backup_destination_dir="/tmp/backup"
 
 RED='\033[0;31m'
-# Check if all
-
-echo "${RED}test"
-echo "test1"
-exit 0;
 
 # Enable nextcloud maintenance mode
-if cd nextcloud_file_dir && sudo -u www-data php occ maintenance:mode --on; then
+if cd $nextcloud_file_dir && sudo -u www-data php occ maintenance:mode --on; then
   echo "Enabled nextcloud maintenance mode"
 else
   echo "Failed enabling nextcloud maintenance mode"
@@ -29,27 +24,25 @@ if [ ! -d $backup_destination_dir ]; then
 fi
 
 # Do backup
-echo "Starting nextcloud file directory backup"
-tar -cpzf /tmp/backup/NextcloudBackup_FileDir_`date +"%Y%m%d-%H-%M-%S"`.tar.gz -C "$nextcloud_file_dir" .
-echo "Finished nextcloud file directory backup"
-
-echo "Starting nextcloud data directory backup"
-tar -cpzf /tmp/backup/NextcloudBackup_DataDir_`date +"%Y%m%d"`.tar.gz -C "$nextcloud_data_dir" .
-echo "Finished nextcloud data directory backup"
-
 echo "Starting nextcloud db backup"
 mysqldump --single-transaction -h localhost -u "$nextcloud_db_user" -p "$nextcloud_db_password" "$nextcloud_db" > /tmp/backup/NextcloudBackup_DB_`date +"%Y%m%d"`.sql
 echo "Finished nextcloud db backup"
 
+echo "Starting nextcloud file directory backup"
+tar -cpzf /tmp/backup/NextcloudBackup_FileDir_`date +"%Y%m%d-%H-%M-%S"`.tar.gz -C "$nextcloud_file_dir" .
+echo "Finished nextcloud file directory backup"
 
 # Disable nextcloud maintenance mode
-if cd nextcloud_file_dir && sudo -u www-data php occ maintenance:mode --off; then
+if cd $nextcloud_file_dir && sudo -u www-data php occ maintenance:mode --off; then
   echo "Disabled nextcloud maintenance mode"
 else
   echo "Failed disabling nextcloud maintenance mode, you have to check if it still enabled!"
   exit 1
 fi
 
+echo "Starting nextcloud data directory backup"
+tar -cpzf /tmp/backup/NextcloudBackup_DataDir_`date +"%Y%m%d"`.tar.gz -C "$nextcloud_data_dir" .
+echo "Finished nextcloud data directory backup"
 
 echo "Finished backup for Nextcloud"
 echo "You can find you backups in: $backup_destination_dir"
